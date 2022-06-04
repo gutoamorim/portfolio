1d38658acfd58e2ccd9f9701d3f7698b22e19ef1
Cesar Augusto <gutoamorim@outlook.com>
2022-06-04 01:09:46 -0300

    menu mobile

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ $(document).ready(function(){
     }
   })
 });
+
+const btnMobile = document.querySelector('#btn-mobile')
+
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefault()
+  const nav = document.querySelector('nav')
+  nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
