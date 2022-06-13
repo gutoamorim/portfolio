1bcce30159bb599969ca3d452f94f78c0c6bd031
Cesar Augusto <gutoamorim@outlook.com>
2022-06-13 16:49:07 -0300

    Corrigindo acessibilidade mobile

--- script.js
+++ script.js
@@ -29,7 +29,15 @@ const nav = document.querySelector('nav')
 
 function toggleMenu(event) {
   if(event.type === 'touchstart') event.preventDefault()
+  const nav = document.querySelector('nav')
   nav.classList.toggle('active')
+  const active = nav.classList.contains('active')
+  event.currentTarget.setAttribute('aria-expanded', active)
+  if(active) {
+    event.currentTarget.setAttribute('aria-label' , 'Fechar menu')
+  } else {
+    event.currentTarget.setAttribute('aria-label' , 'Abrir menu')
+  }
 }
 
 btnMobile.addEventListener('click', toggleMenu)
