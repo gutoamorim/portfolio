1c7c2c1523fed1bf29159b0cca5a1c0a066e7890
Cesar Augusto <gutoamorim@outlook.com>
2022-06-14 09:30:19 -0300

    novo teste de action deploy ftp

--- form.js
+++ form.js
@@ -6,7 +6,7 @@ function deuRuim() {
 
 function formularioEnviado(resposta) {
   if(resposta.ok) {
-    formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1 / -1; padding: 1rem; border-radius: 4px; background: #f7f7f7"> <span style="color: #317a00">Sera que agora foi?!</span> Em breve entraremos em contato. Geralmente respondemos em até 24h</p>'
+    formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1 / -1; padding: 1rem; border-radius: 4px; background: #f7f7f7"> <span style="color: #317a00">Será que agora foi?!</span> Em breve entraremos em contato. Geralmente respondemos em até 24h</p>'
   }else {
     formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1 / -1; padding: 1rem; border-radius: 4px; background: #f7f7f7"><span style="color:#E00000">Erro no envio,</span> você pode enviar diretamente para o nosso e-mail em: bikcraft@cesaraugusto.dev</p>'
   }
