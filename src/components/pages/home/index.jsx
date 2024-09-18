f235408ea9a8799e947d977ce30dc01b5635b369
Cesar Augusto <gutoamorim@outlook.com>
2024-09-18 16:09:01 -0300

    atualização de efeitos da home

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import avatar from "../../../assets/avatar.png";
 import { Button } from "../../button";
-
-import { TbBrandGithub } from "react-icons/tb";
-import { TbBrandLinkedin } from "react-icons/tb";
-import { TbBrandWhatsapp } from "react-icons/tb";
-import { TbMail } from "react-icons/tb";
-
+import {
+  TbBrandGithub,
+  TbBrandLinkedin,
+  TbBrandWhatsapp,
+  TbMail,
+} from "react-icons/tb";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer"; // Use a biblioteca correta
+import { useRef } from "react";
 
 const SOCIAL = [
   {
@@ -28,30 +30,37 @@ const SOCIAL = [
 ];
 
 export const Home = () => {
+  const { ref: ref1, inView: inView1 } = useInView({
+    triggerOnce: true,
+  });
+
+  const { ref: ref2, inView: inView2 } = useInView({
+    triggerOnce: true,
+  });
+
   return (
-    <sectioon
+    <section
       className="w-full min-h-dvh bg-hero-image bg-cover bg-center bg-no-repeat flex mt-16 lg:mt-0"
       id="home"
     >
       <div className="container flex items-center justify-center pt-10 gap-5 md:justify-between flex-col-reverse md:flex-row md:gap-6">
         <motion.div
+          ref={ref1}
           className="w-full lg:max-w-[530px]"
           initial={{ opacity: 0, x: -100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
+          animate={inView1 ? { opacity: 1, x: 0 } : {}}
           transition={{ duration: 0.5 }}
         >
           <p className="font-mono text-emerald-400">Olá, meu nome é</p>
           <h2 className="text-4xl font-medium mt-2">Cesar Augusto</h2>
           <p className="text-gray-400 my-6 text-sm sm:text-base">
-            Sou um desenvolvedor front-end apaixonado por tecnologia. <br />{" "}
+            Sou um desenvolvedor front-end apaixonado por tecnologia. <br />
             Adoro desafios, e o fato de poder ajudar ou facilitar a vida de
             alguém, me deixa animado. Sempre gostei de pensar em como resolver
             problemas e agora posso fazer isso com linhas de código. <br />
             Atualmente curso Análise e Desenvolvimento de Sistemas com previsão
             de formação em julho/2025.
           </p>
-
           <div className="flex flex-col items-center md:items-start">
             <div className="flex items-center gap-4">
               <a href="#contact">
@@ -64,7 +73,6 @@ export const Home = () => {
                 <Button>Download CV</Button>
               </a>
             </div>
-
             <div className="text-3xl text-gray-600 flex items-center gap-3 h-20">
               {SOCIAL.map((item, index) => (
                 <a
@@ -81,9 +89,9 @@ export const Home = () => {
         </motion.div>
 
         <motion.div
+          ref={ref2}
           initial={{ opacity: 0, y: 200, scale: 0.5 }}
-          whileInView={{ opacity: 1, y: 0, scale: 1 }}
-          exit={{ opacity: 0, y: 200, scale: 0.5 }}
+          animate={inView2 ? { opacity: 1, y: 0, scale: 1 } : {}}
           transition={{ duration: 0.5 }}
           className="origin-center"
         >
@@ -94,6 +102,6 @@ export const Home = () => {
           />
         </motion.div>
       </div>
-    </sectioon>
+    </section>
   );
 };
