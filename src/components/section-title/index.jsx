82c4a0fe8f16de9710a06d4fc02fa76bf301d7c3
Cesar Augusto <gutoamorim@outlook.com>
2024-09-18 16:29:45 -0300

    atualização de efeitos da section-title

--- src/components/section-title/index.jsx
+++ src/components/section-title/index.jsx
@@ -1,25 +1,32 @@
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-export const SectionTitle = ({ title, subtitle, classname }) => {
-  const animeProps = {
-    initial: { opacity: 0, x: -100 },
-    whileInView: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: -100 },
-  };
+export const SectionTitle = ({ title, subtitle }) => {
+  const { ref: ref1, inView: inView1 } = useInView({
+    triggerOnce: true,
+  });
+
+  const { ref: ref2, inView: inView2 } = useInView({
+    triggerOnce: true,
+  });
 
   return (
     <div className="flex flex-col gap-4">
       <motion.span
+        ref={ref1}
         className="font-mono text-sm text-emerald-400"
-        {...animeProps}
+        initial={{ opacity: 0, x: -100 }}
+        animate={inView1 ? { opacity: 1, x: 0 } : {}}
         transition={{ duration: 0.5 }}
       >
         {`../${subtitle}`}
       </motion.span>
       <motion.h3
+        ref={ref2}
         className="text-3xl font-medium"
-        {...animeProps}
-        transition={{ duration: 0.5, delay: 0.2 }}
+        initial={{ opacity: 0, x: -100 }}
+        animate={inView2 ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 0.5 }}
       >
         {title}
       </motion.h3>
